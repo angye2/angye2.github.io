7f4f444a6d1ac93c4251a4b0d99167c5dd0edd8a
angye2 <angye.jhoana@gmail.com>
2016-06-21 17:20:12 -0500

    subi proyecto

--- sketch.js
+++ sketch.js
@@ -1,58 +1,86 @@
-var colors = [];
-var size;
-var oscilador;
-var notes = [];
+
+
+var bubbles = [];
+
 
 function setup() {
-	//createCanvas(800,600);
-	//size = 100;
-	createCanvas(window.innerWidth, window.innerHeight);
-	colors [0]= color(255,160,39);
-	colors [1]= color(0,142,255);
-	colors [2]= color(255,16,0);
-	colors [3]= color(255,16,0);
-	colors [4]= color(255,226,11);
-	frameRate(60);
-	size = 100;
-
-oscilador = new p5.Oscillator();
-oscilador.setType('sine');
-oscilador.freq(100);
-oscilador.amp(0.2);
-oscilador.start();
-
-
-notes = [82.41,98.00,55.00,246.94,61.74,36.71,82.41]; 
-
-delay = new p5.Delay();
-delay.process(oscilador, .12, .7, 2300);
-}
-
-
-
-
-function draw() {
+	createCanvas(600,400);	
 	
-	//background(225);
-
-	if (size<100){size++;}
-	if (size==100){size=1;}
-		fill(225,3);
-		rect(0,0,width,height);
-
-	if(mouseIsPressed){
-		fill(colors[parseInt(random(5))]);
-	}else fill(255);
-	//size =  random(100);
-  	ellipse(mouseX,mouseY,size,size);
-  	ellipse(width-mouseX,mouseY,size,size);
-	ellipse(mouseX,height-mouseY,size,size);
-  	ellipse(width-mouseX,height-mouseY,size,size);
-
-  	if (mouseX>0&&mouseX<=width/6){oscilador.freq(notes[0]);}
-	if (mouseX>width/6.&&mouseX<=2*width/6){oscilador.freq(notes[1]);}
-	if (mouseX>2*width/6.&&mouseX<=3*width/6){oscilador.freq(notes[2]);}
-	if (mouseX>3*width/6.&&mouseX<=4*width/6){oscilador.freq(notes[3]);}
-	if (mouseX>5*width/6.&&mouseX<=6*width/6){oscilador.freq(notes[4]);}
 }
 
+ function draw() {
+
+ 	background(0);
+ 	
+ 	for (var i = 0; i<bubbles.length;i++){
+
+ 		for (var j =0;j <bubbles.length; j++) {
+ 			if (i!=j & bubbles[i].intersects(bubbles[j])) {
+ 				bubbles[i].speedX=bubbles[i].speedX*-1;
+ 				bubbles[i].speedY=bubbles[i].speedY*-1;
+ 			}
+ 		}	
+ 	}
+
+ 	for (var j =0;j <bubbles.length; j++) {
+ 			
+ 				bubbles[j].move();
+ 				bubbles[j].display();
+ 			
+ 		}	
+
+ 	
+ }
+
+ function mousePressed(){
+ 	bubbles.push(new Bubble(mouseX,mouseY));
+ }
+
+
+ function Bubble(x,y) {
+
+ 	this.x=x;
+ 	this.y=y;
+ 	this.r=random(10,20);
+ 	
+ 	this.speedX = random(-2,2);
+ 	this.speedY = random(-2,2);
+
+ 	this.display=function() {
+ 		ellipse(this.x,this.y,this.r*2,this.r*2);
+
+
+ 	}
+ 
+
+ 	this.move = function() {
+
+ 		this.x=this.x+this.speedX;
+ 		this.y=this.y+this.speedY;
+ 		this.lifesapan = this.lifesapan - 1;
+
+ 		if ((this.x > width) || (this.x < 0)) {
+ 			this.speedX = this.speedX * -1;
+ 		}
+
+ 		if ((this.y > height) || (this.y <0)) {
+ 			this.speedY = this.speedY * -1;
+ 		}	
+
+ 	}
+
+ 	this.intersects = function(bub) {
+
+ 		 d = dist(this.x, this.y, bub.x, bub.y);
+ 		if (d < this.r + bub.r) {
+ 			return true;
+ 		}   else {
+ 			return false;
+ 		}
+ 	}
+
+ }
+
+
+
+ 
